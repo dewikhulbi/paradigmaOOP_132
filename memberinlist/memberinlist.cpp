411accf260a153e9884f80e87d07d225b75c9441
dewikhulbi <dewikhlb20@gmail.com>
2023-05-22 18:57:25 +0700

    mengembalikan nilai

--- memberinlist/memberinlist.cpp
+++ memberinlist/memberinlist.cpp
@@ -17,4 +17,11 @@ public:
         cout << "Nama  = " << nama << endl;
         cout << "Nilai = " << nilai << endl;
     }
+};
+
+int main() {
+    mahasiswa mhs(12, "Asroni", 90.5);
+
+    return 0;
+}
 
