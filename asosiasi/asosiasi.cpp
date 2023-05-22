bce8c54b7d31b7252a7c4eac031fb7f6d56477e6
dewikhulbi <dewikhlb20@gmail.com>
2023-05-22 18:27:46 +0700

    memasukan nama dokter dan pasien

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -38,3 +38,24 @@ void pasien::tambahDokter(dokter* pDokter) {
 }
 void pasien::cetakDokter() {
     cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+void dokter::cetakPasien() {
+    cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+int main() {
+    dokter* varDokter1 = new dokter("dr.Budi");
+    dokter* varDokter2 = new dokter("dr.Tono");
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
